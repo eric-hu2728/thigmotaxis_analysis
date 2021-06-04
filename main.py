7d51aff7af00635245ec7daf62d1c332149ae448
eric-hu2728 <72859331+eric-hu2728@users.noreply.github.com>
2021-06-04 15:00:13 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import itertools
+import math
+from numpy import genfromtxt
+from itertools import combinations
+from math import hypot
+#currently, the user has to pre-format their xy data output as a 2-column csv.
+raw_data = genfromtxt('/home/eo/Desktop/xy.csv', delimiter=',')
+#convert from csv into a python-readable data array. Print it out to confirm.
+coords = np.array(raw_data)
+print (coords)
+#define each xy pair as a point, and find the distance between each point and every other point (n choose 2)
+def distance(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return hypot(x2 - x1, y2 - y1)
+dist = [distance(*combo) for combo in combinations(coords, 2)]
+print(dist)
+#Simply sums all the inter-individual distance, then takes the arithmetic mean.
+len(dist)
+print(np.mean(dist))
