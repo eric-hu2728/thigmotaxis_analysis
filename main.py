14e15a6b8e5a48bd84cdda4a4e16a0f8d25d780b
Eric Odle <72859331+eric-hu2728@users.noreply.github.com>
2021-06-06 13:55:37 +0800

    Update main.py
    Added csv output of results

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ import math
 from numpy import genfromtxt
 from itertools import combinations
 from math import hypot
-#currently, the user has to pre-format their xy data output as a 2-column csv. Working on computer vision feature to auto-determine this from an image file.
-raw_data = genfromtxt('file.csv', delimiter=',')
-#convert from csv into a python-readable data array. Print it out to confirm.
+# Currently, the user has to pre-format their xy data output as a 2-column csv.
+raw_data = genfromtxt('+path/testdata.csv', delimiter=',')
+# Convert from csv into a python-readable data array. Print it out to confirm.
 coords = np.array(raw_data)
-print (coords)
-#define each xy pair as a point, and find the distance between each point and every other point (n choose 2)
+# Define each xy pair as a point, and find the distance between each point and every other point (n choose 2)
 def distance(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return hypot(x2 - x1, y2 - y1)
 dist = [distance(*combo) for combo in combinations(coords, 2)]
-print(dist)
-#Simply sums all the inter-individual distance, then takes the arithmetic mean.
+#Simply sums all the inter-individual distance, then takes the arithmetic mean
 len(dist)
-print(np.mean(dist))
+result = np.mean(dist)
+# Output to csv
+import pandas as pd
+dict={'result', result}
+df = pd.DataFrame(dict)
+df.to_csv('~path/output.csv')
